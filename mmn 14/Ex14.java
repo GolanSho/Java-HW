18c4e51d69d1a2406ed1a505e15600616562a3c3
GolanSho <46089718+GolanSho@users.noreply.github.com>
2020-06-10 13:57:15 +0300

    task 2

--- mmn 14/Ex14.java
+++ mmn 14/Ex14.java
@@ -53,4 +53,47 @@ public class Ex14{
         else
             return arr[end];
     }
+
+    public static int findTriplet (int [] arr){
+        int biggestNum1 = 0;
+        int biggestNum2 = 0;
+        int smallestNum1 = 0;
+        int smallestNum2 = 0;
+        int biggestNum3 = 0;
+        
+        for (int i = 0; i < arr.length; i++){
+            
+            if (arr[i] > biggestNum3){
+                if (arr[i] > biggestNum2) {
+                    if (arr[i] > biggestNum1){
+                        biggestNum3 = biggestNum2;
+                        biggestNum2 = biggestNum1;
+                        biggestNum1 = arr[i];
+                    }
+                    else{
+                        biggestNum3 = biggestNum2;
+                        biggestNum2 = arr[i];
+                    }
+                }
+                else
+                    biggestNum3 = arr[i];
+            }
+            if (arr[i] < smallestNum2){
+                if (arr[i] < smallestNum1){
+                    smallestNum2 = smallestNum1;
+                    smallestNum1 = arr[i];
+                }
+                else
+                    smallestNum2 = arr[i];
+            }
+        }
+        if ((biggestNum1 * biggestNum2 * biggestNum3) >= (smallestNum1 * smallestNum2 * biggestNum1)){
+            System.out.println(biggestNum1 + " " + biggestNum2 + " " + biggestNum3);
+            return (biggestNum1 * biggestNum2 * biggestNum3);
+        }
+        else {
+            System.out.println(smallestNum1 + " " + smallestNum2 + " " + biggestNum1);
+            return (smallestNum1 * smallestNum2 * biggestNum1);
+        }
+    }
 }
--- mmn 14/Tester.java
+++ mmn 14/Tester.java
@@ -1,10 +1,13 @@
 public class Tester {
     public static void main(String args[]){
         int [] arr = new int[8];
+        int [] triple = {-4, 1, -8, 9, 6};
+        int [] triple2 = {2, 10, -5, 4, -6, 0, -6, 5};
         int[] coins1 = {16,23,30,13,14,21,19,15};
         for (int i = 0; i < arr.length; i++){
             arr[i] = (int)(Math.random()*100);
         }
-        Ex14.win(coins1);
+        //Ex14.win(coins1);
+        Ex14.findTriplet(triple2);
     }
 }
