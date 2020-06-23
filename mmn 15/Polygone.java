bbcb8cb6a5eeb555889676a8ffe0f88119f70382
GolanSho <46089718+GolanSho@users.noreply.github.com>
2020-06-23 20:29:11 +0300

    ex15

---
+++ mmn 15/Polygone.java
@@ -0,0 +1,14 @@
+class Polygone {
+    private Point _head;
+
+    public Polygone() {
+        _head = null;
+    }
+
+    public boolean addVertex(Point p, int pos){
+        if(_head != null && pos > 0){
+
+        }
+        return false;
+    }
+}
